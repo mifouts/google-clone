f423fd9226261d7d48479315fca8e2ebe98ab5bb
mifouts <fouts.mitch15@gmail.com>
2022-11-29 16:36:07 -0500

    starting home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-
+import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
       <h1>Let's build the google clone</h1>
+
+      {/* home page*/}
+      <Home />
+      {/* search results page*/}
     </div>
   );
 }
