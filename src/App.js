72e85ac8652c43577c0ee74109f9adb6fe774cd2
mifouts <fouts.mitch15@gmail.com>
2022-11-30 10:03:21 -0500

    had to use routes instead of switch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
+        <Routes>
           <Route path="/search">
             <h1>Search Page</h1>
           </Route>
           <Route path="/">
             <Home />
           </Route>
-        </Switch>
+        </Routes>
       </Router>
     </div>
   );
