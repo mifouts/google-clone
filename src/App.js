48ca16fd36dd608eda9d771cc12ab015d1cec5cb
mifouts <fouts.mitch15@gmail.com>
2022-11-30 09:54:41 -0500

    installing packages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import React from 'react';
 import './App.css';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
 function App() {
   return (
     <div className="App">
+      <Router>
       <h1>Let's build the google clone</h1>
 
       {/* home page*/}
       <Home />
       {/* search results page*/}
+      </Router>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,19 @@ import './Home.css';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <h1>This is HOMEPAGE</h1>
+
+      <div className="home__header">
+        <div className="header__headerLeft">
+            
+        </div>
+        <div className="header__headerRight">
+
+        </div>
+      </div>
+      <div className="home__body"></div>
+      </div>
   )
 }
 
