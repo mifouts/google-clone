2d98d9c8086675dff4b7d5557a6ce620f9d7eb19
mifouts <fouts.mitch15@gmail.com>
2022-12-02 14:50:16 -0500

    making search bar work

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import MicIcon from "@mui/icons-material/Mic";
 import "./Search.css";
 import { Button } from "@mui/material";
 
 function Search() {
+  const [input, setInput] = useState("");
+
+  const search = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="search">
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
-        <input type="text" />
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
         <MicIcon />
       </div>
       <div className="search__buttons">
-        <Button variant="outlined">Google Search</Button>
+        <Button onClick={search} variant="outlined">
+          Google Search
+        </Button>
         <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
     </div>
