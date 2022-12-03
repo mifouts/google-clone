a7439bb5df234969afee5ae01e8ab306429f4768
mifouts <fouts.mitch15@gmail.com>
2022-12-03 14:58:28 -0500

    creating search page component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SearchPage from "./components/SearchPage";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/search" />
+          <Route path="/search" element={<SearchPage />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
