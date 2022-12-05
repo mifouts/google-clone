406a3575b80e6a3e5d5c8b046fb152fbbc966f33
mifouts <fouts.mitch15@gmail.com>
2022-12-05 14:48:13 -0500

    connecting google api

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -10,8 +10,13 @@ const useGoogleSearch = (term) => {
     const fetchData = async () => {
       fetch(
         `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
-      );
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setData(result);
+        });
     };
+    fetchData();
   }, [term]);
 
   return { data };
